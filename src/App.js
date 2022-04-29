faea54119dddaa1a819f870bbe53571871fae8f6
Jim-Tony <jimtonystarc@gmail.com>
2022-04-29 19:51:01 +0530

    added pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+import Home from './components/pages/Home';
 function App() {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar/>
-        <main>content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/notfound' element={<NotFound/>}/>
+            <Route path='/*' element={<NotFound/>}/>
+          </Routes>
+        </main>
         <Footer/>
       </div>
     </Router>
