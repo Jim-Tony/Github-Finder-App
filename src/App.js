d9b7566ffd1846bdd716a0b5747100de2dc4e161
Jim-Tony <jimtonystarc@gmail.com>
2022-05-04 00:28:57 +0530

    search comp

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 import Home from './components/pages/Home';
+import {GithubProvider} from './context/github/GithubContext';
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar/>
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/notfound' element={<NotFound/>}/>
-            <Route path='/*' element={<NotFound/>}/>
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar/>
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/notfound' element={<NotFound/>}/>
+              <Route path='/*' element={<NotFound/>}/>
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,30 +1,12 @@
-import {useState,useEffect} from 'react';
+import {useContext} from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
 function UserResults() {
-    const [users,setUsers] = useState([]);
-    const [loading,setLoading] = useState(true);
-    useEffect(()=>{
-        fetchUsers();
-    },[])
-
-    const fetchUsers = async ()=>{
-        // console.log("ishap");
-        const response = await fetch(`https://api.github.com/users`,{
-            // method:'GET',
-            headers: {
-                Authorization: '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'
-            }}
-            );
-        console.log(response);
-
-        const data = await response.json();
-        // console.log(data);
-        setUsers(data);
-        setLoading(false);
-    }
+    const {users,loading} = useContext(GithubContext);
     if(!loading) return (
         <div className="grid mx-auto grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-            {users.map((user)=>(<h3 key={user.id}>{user.login}</h3>))}
+            {users.map(user=><UserItem key={user.id} user={user}/>)}
         </div>
     )
     else return <Spinner/>
